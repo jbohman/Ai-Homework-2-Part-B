9edfe765486165ba4d14ab60c67441f0ac020cb1
Linus Wallgren <linuswa@csc.kth.se>
2010-09-16 13:48:54 +0200

    Updated diaconstraint

--- src/DiaConstraint.java
+++ src/DiaConstraint.java
@@ -3,9 +3,7 @@ public class DiaConstraint implements Constraint {
 
 	@Override
 	public boolean isValid(Amazon a, Amazon b) {
-		double diaA = (a.getRow()+1) / (a.getCol()+1);
-		double diaB = (b.getRow()+1) / (b.getCol()+1);
-		if (diaA != diaB)
+		if (Math.abs(a.getRow() - b.getRow()) != Math.abs(a.getCol() - b.getCol()))
 			return true;
 		return false;
 	}

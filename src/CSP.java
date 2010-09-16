d706de37923743368e39dbc995e99a4d51d2c989
Linus Wallgren <linuswa@csc.kth.se>
2010-09-16 20:58:40 +0200

    Fixesd javadocs and cleand up a bit

--- src/CSP.java
+++ src/CSP.java
@@ -1,131 +1,195 @@
 import java.util.*;
 
-
 public class CSP {
-	public short board;
+    public short board;
 
-	short matrix[];
+    short matrix[];
 
-	public CSP(short board) {
-		this.board = board;
-		
-		matrix = new short[board];
-		
-		for (short i = 0; i < board; ++i) {
-			matrix[i] = (short)((i * 3) % board);
-		}
+    /**
+     * Initialise the matrix with our amazons
+     * 
+     * @param board
+     *            The board size
+     */
+    public CSP(short board) {
+	this.board = board;
+
+	matrix = new short[board];
+
+	for (short i = 0; i < board; ++i) {
+	    matrix[i] = (short) ((i * 3) % board);
 	}
-	
-	public boolean solution() {
-		for (short i = 0; i < board; ++i) {
-			if (isConflicted(i))
-				return false;
-		}
-		return true;
-	}
-	
-	/**
-	 * 
-	 */
-	public boolean isConflicted(short col) {
-		for (short i = 0; i < board; ++i) {
-			if (!equal(i, col)) {
-				if (conflicts(col, matrix[col], i, matrix[i]))
-					return true;
-			}
-		}
+    }
+
+    /**
+     * Is the problem solved?
+     * 
+     * @return True if there is no conflicting amazons.
+     */
+    public boolean solution() {
+	for (short i = 0; i < board; ++i) {
+	    if (isConflicted(i))
 		return false;
 	}
-	
-	public boolean equal(short aCol, short bCol) {
-		if (aCol == bCol && matrix[aCol] == matrix[bCol])
-			return true;
-		return false;
-	}
+	return true;
+    }
 
-	/**
-	 * 
-	 * @param amazon
-	 * @return The row at which there is the least amount of conflicts
-	 */
-	public short minConflicts(short col) {
-		ArrayList<Short> lista = new ArrayList<Short>();
-		short conflicts = Short.MAX_VALUE;
-		
-		for (short i = 0; i < board; ++i) {
-			if (i != matrix[col]) {
-				
-				short tmp = numConflicts(col, i);
-				if (tmp <= conflicts) {
-					if (tmp != conflicts) {
-						lista.clear();
-					}
-					lista.add(i);
-					conflicts = tmp;
-				}
-			}
-		}
-		return lista.get((short)(Math.random()*lista.size()));
+    /**
+     * Check if a amazon is in conflict.
+     * 
+     * @param col
+     *            The column corresponding to the amazon
+     * @return true if there is a conflict.
+     */
+    public boolean isConflicted(short col) {
+	for (short i = 0; i < board; ++i) {
+	    if (!equal(i, col)) {
+		if (conflicts(col, matrix[col], i, matrix[i]))
+		    return true;
+	    }
 	}
-	
+	return false;
+    }
 
-	public short numConflicts(short col, short newRow) {
-		short conflict = 0;
-		
-		for (short i = 0; i < board; ++i) {
-			if (matrix[i] != matrix[col]) {
-				conflict += numConflicts(col, newRow, i, matrix[i]);
-			}
-			
+    /**
+     * Used to check if two amazon columns are the same
+     * 
+     * @param aCol
+     *            The ID of the first amazon
+     * @param bCol
+     *            The ID of the second amazon
+     * @return true if the amazons are equal.
+     */
+    public boolean equal(short aCol, short bCol) {
+	if (aCol == bCol && matrix[aCol] == matrix[bCol])
+	    return true;
+	return false;
+    }
+
+    /**
+     * Gives the row with the smallest amount of conflicts for the amazon. The
+     * return value is randomised against the rows with the same amount of
+     * conflicts.
+     * 
+     * @param col
+     *            The amazon
+     * @return One of the rows with the smallest amount of conflicts
+     */
+    public short minConflicts(short col) {
+	ArrayList<Short> lista = new ArrayList<Short>();
+	short conflicts = Short.MAX_VALUE;
+
+	for (short i = 0; i < board; ++i) {
+	    if (i != matrix[col]) {
+
+		short tmp = numConflicts(col, i);
+		if (tmp <= conflicts) {
+		    if (tmp != conflicts) {
+			lista.clear();
+		    }
+		    lista.add(i);
+		    conflicts = tmp;
 		}
-		return conflict;
+	    }
 	}
-	
-	
-	boolean conflicts(short aCol, short aRow, short bCol, short bRow) {
-		// row
-		if (aRow == bRow)
-			return true;
-		// col
-		if (aCol == bCol)
-			return true;
-		// dia
-		if (Math.abs(aRow - bRow ) == Math.abs(aCol - bCol))
-			return true;
-//		// box
-		if (Math.abs(aCol - bCol) <= 2 && Math.abs(aRow - bRow) <= 2)
-			return true;
-		return false;
+	return lista.get((short) (Math.random() * lista.size()));
+    }
+
+    /**
+     * The number of conflicts for a given amazon at a given row.
+     * 
+     * @param col
+     *            The ID of the amazon
+     * @param newRow
+     *            The row to try
+     * @return The number of conflicts
+     */
+    public short numConflicts(short col, short newRow) {
+	short conflict = 0;
+
+	for (short i = 0; i < board; ++i) {
+	    if (matrix[i] != matrix[col]) {
+		conflict += numConflicts(col, newRow, i, matrix[i]);
+	    }
+
 	}
-	
-	byte numConflicts(short aCol, short aRow, short bCol, short bRow) {
-		byte tmp = 0;
-		// row
-		if (aRow == bRow)
-			tmp++;
-		// col
-		if (aCol == bCol)
-			tmp++;
-		// dia
-		if (Math.abs(aRow - bRow ) == Math.abs(aCol - bCol))
-			tmp++;
-//		// box
-		if (Math.abs(aCol - bCol) <= 2 && Math.abs(aRow - bRow) <= 2)
-			tmp++;
-		return tmp;
+	return conflict;
+    }
+
+    /**
+     * Returns true if two amazons conflict
+     * 
+     * @param aCol
+     *            The column (ID) of the first amazon
+     * @param aRow
+     *            The row of the first amazon
+     * @param bCol
+     *            The column (ID) of the second amazon
+     * @param bRow
+     *            The row of the second amazon
+     * @return true if they conflict in some way.
+     */
+    boolean conflicts(short aCol, short aRow, short bCol, short bRow) {
+	// row
+	if (aRow == bRow)
+	    return true;
+	// col
+	if (aCol == bCol)
+	    return true;
+	// dia
+	if (Math.abs(aRow - bRow) == Math.abs(aCol - bCol))
+	    return true;
+	// box
+	if (Math.abs(aCol - bCol) <= 2 && Math.abs(aRow - bRow) <= 2)
+	    return true;
+	return false;
+    }
+
+    /**
+     * Returns the number of times two amazons conflict
+     * 
+     * @param aCol
+     *            The column (ID) of the first amazon
+     * @param aRow
+     *            The row of the first amazon
+     * @param bCol
+     *            The column (ID) of the second amazon
+     * @param bRow
+     *            The row of the second amazon
+     * @return The number of conflicts between the amazons
+     */
+    byte numConflicts(short aCol, short aRow, short bCol, short bRow) {
+	byte tmp = 0;
+	// row
+	if (aRow == bRow)
+	    tmp++;
+	// col
+	if (aCol == bCol)
+	    tmp++;
+	// dia
+	if (Math.abs(aRow - bRow) == Math.abs(aCol - bCol))
+	    tmp++;
+	// // box
+	if (Math.abs(aCol - bCol) <= 2 && Math.abs(aRow - bRow) <= 2)
+	    tmp++;
+	return tmp;
+    }
+
+    /**
+     * Format the output in an human readable way. NOTE that the rows and
+     * columns might be swapped.
+     */
+    public String toString() {
+	String tmp = "";
+	for (short i = 0; i < board; ++i) {
+	    for (short j = 0; j < board; ++j) {
+		if (matrix[i] == j)
+		    tmp += "A ";
+		else
+		    tmp += ". ";
+	    }
+	    tmp += "\n";
 	}
-	
-	public String toString() {
-		String tmp = "";
-		for (short i = 0; i < board; ++i) {
-			for (short j = 0; j < board; ++j) {
-				if (matrix[i] == j)
-					tmp += "A ";
-				else
-					tmp += ". ";
-			}
-			tmp += "\n";
-		}
-		return tmp;
-	}
+	return tmp;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,95 @@
-import java.util.Collections;
 import java.util.Random;
 
-
 public class Main {
-	static Random rng = new Random();
+    static Random rng = new Random();
 
-	private boolean DEBUG = true;
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		new Main();
-	}
-	
-	public Main() {
-		if (!DEBUG) {
-			for (short i = 10; i <= 1000; i += 10) {
-				short fails = 0;
-				long start = System.currentTimeMillis();
-				for (short j = 0; j < 1; ++j) {
-					CSP csp = new CSP(i);
-					if (minConflicts(csp, (short)1000) == -1)
-						fails++;
-				}
-				System.out.println(i + ":\t" + fails + " took " + 
-						(System.currentTimeMillis()-start));
-			}
-		}
-		else {
-			long start = System.currentTimeMillis();
-			CSP csp = new CSP((short)1000);
-			System.out.println(minConflicts(csp, (short)1000) + "\t took " + 
-					(System.currentTimeMillis()-start));
-		}
-	}
-	
-	private short minConflicts(CSP csp, short maxSteps) {
-		
-		short shuffleList[] = new short[csp.board];
-		for (short i = 0; i < csp.board; ++i) {
-			shuffleList[i] = i;
-		}
-		
-		for (short i = 0; i < maxSteps; ++i) {
-			if (csp.solution()) {
-				return i;
-			}
-			
-			shuffle(shuffleList);
-			
-			for (short n = 0; n < csp.board; ++n) {
-				if (csp.isConflicted(shuffleList[n])) {
-					short moveTo = csp.minConflicts(shuffleList[n]);
-					
-					csp.matrix[shuffleList[n]] = moveTo;
-					
+    private boolean DEBUG = false;
 
-//					break;
-				}
-			}	
-		}
-		return -1;
-	}
-	
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	new Main();
+    }
 
-	public static void shuffle(short[] array) {
-	    // i is the number of items remaining to be shuffled.
-	    for (int i = array.length; i > 1; i--) {
-	        // Pick a random element to swap with the i-th element.
-	        short j = (short) rng.nextInt(i);  // 0 <= j <= i-1 (0-based array)
-	        // Swap array elements.
-	        short tmp = array[j];
-	        array[j] = array[i-1];
-	        array[i-1] = tmp;
+    /**
+     * Run the actual code
+     */
+    public Main() {
+	if (!DEBUG) {
+	    for (short i = 50; i <= 1000; i += 50) {
+		short fails = 0;
+		long start = System.currentTimeMillis();
+		// for (short j = 0; j < 1; ++j) {
+		CSP csp = new CSP(i);
+		short iterations = minConflicts(csp, (short) 1000);
+		if (iterations == -1)
+		    fails++;
+		// }
+		System.out.println(i + ":\t" + iterations
+			+ " \titerations took \t "
+			+ (System.currentTimeMillis() - start) + " ms");
+	    }
+	} else {
+	    long start = System.currentTimeMillis();
+	    CSP csp = new CSP((short) 1000);
+	    System.out.println(minConflicts(csp, (short) 1000) + "\t took "
+		    + (System.currentTimeMillis() - start));
+	}
+    }
+
+    /**
+     * The actual minConflicts algorithm
+     * 
+     * @param csp
+     *            An CSP
+     * @param maxSteps
+     *            The maximum amount of steps to take before bailing
+     * @return The number of steps actually taken, or -1 if we bailed.
+     */
+    private short minConflicts(CSP csp, short maxSteps) {
+
+	short shuffleList[] = new short[csp.board];
+	for (short i = 0; i < csp.board; ++i) {
+	    shuffleList[i] = i;
+	}
+
+	for (short i = 0; i < maxSteps; ++i) {
+	    if (csp.solution()) {
+		return i;
+	    }
+
+	    shuffle(shuffleList);
+
+	    for (short n = 0; n < csp.board; ++n) {
+		if (csp.isConflicted(shuffleList[n])) {
+		    short moveTo = csp.minConflicts(shuffleList[n]);
+
+		    csp.matrix[shuffleList[n]] = moveTo;
+
+		    break;
+		}
 	    }
 	}
+	return -1;
+    }
+
+    /**
+     * A function for shuffling an array.
+     * 
+     * @param array
+     *            The array to be shuffled.
+     */
+    public static void shuffle(short[] array) {
+	// i is the number of items remaining to be shuffled.
+	for (int i = array.length; i > 1; i--) {
+	    // Pick a random element to swap with the i-th element.
+	    short j = (short) rng.nextInt(i); // 0 <= j <= i-1 (0-based array)
+	    // Swap array elements.
+	    short tmp = array[j];
+	    array[j] = array[i - 1];
+	    array[i - 1] = tmp;
+	}
+    }
 
 }
